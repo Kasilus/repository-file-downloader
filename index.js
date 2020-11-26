4c9f4478051d1842596932cd7c21a27b18478923
Kasilus <its.an.omen.man@gmail.com>
2020-11-27 00:58:56 +0200

    Start adding form and receiving

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const modules = require('./modules.js');
 
 let app = express();
 
+// Parse URL-encoded bodies (as sent by HTML forms)
+app.use(express.urlencoded());
+
 app.set('views', path.join(__dirname, 'templates'));
 app.set("view engine", "ejs");
 
@@ -13,7 +16,13 @@ app.get("/", (req, res) => {
   res.render("index", {
         modules: modules.modules
     });
+});
+
+app.post("/download", (req, res) => {
+  console.log('in download');
 })
+
+
 
 var port = 3000;
 app.listen(port, () => {
